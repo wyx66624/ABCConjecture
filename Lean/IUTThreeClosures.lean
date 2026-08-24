74fed5e749e3bc922488f4d4f1b28c54463ef4ce
wyx66624 <96614266+wyx66624@users.noreply.github.com>
2026-08-24 08:10:08 -0700

    Compile the complete GenEll Lemma 4.1 development

--- Lean/IUTThreeClosures.lean
+++ Lean/IUTThreeClosures.lean
@@ -55,6 +55,7 @@ import IUTThreeClosures.ChebyshevMultiplePrimeEscape
 import IUTThreeClosures.BertrandBoundedPrimeEscape
 import IUTThreeClosures.GenEllLemma41ScalarCore
 import IUTThreeClosures.GenEllIntegerEndpointsPNT
+import IUTThreeClosures.CompleteGenEllLemma41
 import IUTThreeClosures.RationalJModuliDegree
 import IUTThreeClosures.PublicFreyTheorem110OutsideFinite
 import IUTThreeClosures.GlobalQPilotReconstruction
